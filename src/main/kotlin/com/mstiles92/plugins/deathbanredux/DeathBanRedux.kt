e01666d8ce6424dcdf6835f98a046aa528a7a00c
mstiles92 <mstiles92@gmail.com>
2015-02-03 20:31:05 -0500

    Initial commit

---
+++ src/main/kotlin/com/mstiles92/plugins/deathbanredux/DeathBanRedux.kt
@@ -0,0 +1,14 @@
+package com.mstiles92.plugins.deathbanredux
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DeathBanRedux() : JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
