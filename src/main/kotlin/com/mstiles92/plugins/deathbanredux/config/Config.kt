d0574b4a86c89200d4ff1f49429c5693c614abb1
mstiles92 <mstiles92@gmail.com>
2015-02-03 22:23:52 -0500

    Add copyright headers to source files.

--- src/main/kotlin/com/mstiles92/plugins/deathbanredux/config/Config.kt
+++ src/main/kotlin/com/mstiles92/plugins/deathbanredux/config/Config.kt
@@ -1,3 +1,26 @@
+/*
+ * This document is a part of the source code and related artifacts for
+ * DeathBanRedux, an open source Bukkit plugin for hardcore-type servers
+ * where players are temporarily banned upon death.
+ *
+ * http://dev.bukkit.org/bukkit-plugins/deathbanredux/
+ * http://github.com/mstiles92/DeathBanRedux
+ *
+ * Copyright (c) 2015 Matthew Stiles (mstiles92)
+ *
+ * Licensed under the Common Development and Distribution License Version 1.0
+ * You may not use this file except in compliance with this License.
+ *
+ * You may obtain a copy of the CDDL-1.0 License at
+ * http://opensource.org/licenses/CDDL-1.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
 package com.mstiles92.plugins.deathbanredux.config
 
 import com.mstiles92.plugins.deathbanredux.DeathBanRedux
